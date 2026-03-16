032fa115d345e46bd1c2d88cb5e283965716ef9a
imouine <67259008+imouine@users.noreply.github.com>
2026-03-16 22:43:58 +0100

    feat: add license and author information to service worker, options CSS, HTML, and JS files

--- src/background/service-worker.js
+++ src/background/service-worker.js
@@ -1,3 +1,11 @@
+/*
+ * Lille Bus Extension
+ * Author: imouine
+ * Copyright (c) 2026
+ * License: GPL-3.0
+ * https://github.com/imouine/lille-bus-extension
+ */
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.action.setBadgeText({ text: "…" });
   chrome.action.setBadgeBackgroundColor({ color: "#1976d2" });
--- src/options/options.js
+++ src/options/options.js
@@ -1,1 +1,9 @@
+/*
+ * Lille Bus Extension
+ * Author: imouine
+ * Copyright (c) 2026
+ * License: GPL-3.0
+ * https://github.com/imouine/lille-bus-extension
+ */
+
 console.log("Lille Bus Extension options page loaded");
